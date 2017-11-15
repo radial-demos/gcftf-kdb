e44644cb44dc4ddcfbb8f25988d4d58f35aaec50
Darren Anderson <dranders@colorado.edu>
2017-11-15 07:31:39 -0800

    Include nation properties in each (child) jurisdiction.

--- config/regions/index.js
+++ config/regions/index.js
@@ -2,7 +2,6 @@
 
 require('dotenv').config();
 const debug = require('debug')('kdb:data-definitions');
-const _ = require('lodash');
 
 const fields = {};
 fields.national = require('../fields/national');
@@ -16,5 +15,18 @@ nations.push(require('./ivorycoast'));
 nations.push(require('./mexico'));
 nations.push(require('./nigeria'));
 nations.push(require('./peru'));
+
+/**
+ * include some nation (parent) properties in each jurisdiction (child)
+ */
+nations.forEach((nation) => {
+  if (!Array.isArray(nation.jurisdictions)) return;
+  nation.jurisdictions.forEach((jurisdiction) => {
+    jurisdiction.nationId = nation.id;
+    jurisdiction.nationName = nation.name;
+    jurisdiction.nationUriSegment = nation.uriSegment;
+    jurisdiction.currency = nation.currency;
+  });
+});
 
 module.exports = nations;
