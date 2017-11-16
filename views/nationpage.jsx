6e3b4e82d940f361171ebf06c240bcce0ed653ba
Darren Anderson <dranders@colorado.edu>
2017-11-16 15:37:59 -0800

    Include all text fields

--- views/nationpage.jsx
+++ views/nationpage.jsx
@@ -2,12 +2,23 @@ require('dotenv').config();
 const debug = require('debug')('kdb:views:nationpage');
 const React = require('react');
 const NationLayout = require('./layouts/nation.jsx');
+const {
+  Divider,
+  EntryText,
+  StatisticString,
+  StatisticContact,
+  StatisticNumber,
+  StatisticNumberAndCurrency,
+  StatisticNumberAndYear,
+} = require('./components');
 
 class Page extends React.Component {
   render() {
     return (
       <NationLayout {...this.props}>
-        <div>Hello {this.props.viewId}</div>
+        <EntryText entry={this.props.data.nation.fields.txt_laws}/>
+        <EntryText entry={this.props.data.nation.fields.txt_policies_and_plans}/>
+        <EntryText entry={this.props.data.nation.fields.txt_institutions}/>
       </NationLayout>
     );
   }
