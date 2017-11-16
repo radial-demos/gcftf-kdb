c5ae12d43f7de8db0ea80b57e19ac80cdc2105bf
Darren Anderson <dranders@colorado.edu>
2017-11-16 07:53:03 -0800

    Include one textEntry sample element in CarbonAccounting and StateOverview views.

--- views/carbonaccounting.jsx
+++ views/carbonaccounting.jsx
@@ -2,15 +2,16 @@ require('dotenv').config();
 const debug = require('debug')('kdb:views:carbonaccounting');
 const React = require('react');
 const JurisdictionalLayout = require('./layouts/jurisdictional.jsx');
+const EntryText = require('./components/entry-text.jsx');
 
-class HelloMessage extends React.Component {
+class Page extends React.Component {
   render() {
     return (
       <JurisdictionalLayout {...this.props}>
-        <div>Hello {this.props.name}</div>
+        <EntryText entry={this.props.jurisdiction.fields.overviewForestMonitoringAndMeasurementSystems}/>
       </JurisdictionalLayout>
     );
   }
 }
 
-module.exports = HelloMessage;
+module.exports = Page;
