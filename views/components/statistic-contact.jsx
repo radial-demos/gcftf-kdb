5feade435d45347e456b0b028a1ef6b2a7912f8c
Darren Anderson <dranders@colorado.edu>
2017-11-18 08:57:46 -0800

    Update Contact component to be fully functional.

--- views/components/statistic-contact.jsx
+++ views/components/statistic-contact.jsx
@@ -4,11 +4,23 @@ const React = require('react');
 
 class Component extends React.Component {
   render() {
+    const entry = this.props.entry || {};
+    const entryValue = entry.value || {};
+    debug(entryValue);
+    const label = entry.label || '';
+    const showLabel = (!(this.props.showLabel === false) && label.length);
+    const nameFull = `${entryValue.firstName.string} ${entryValue.lastName.string}`.trim();
+    const email = entryValue.email.string;
+    const companyTitle = entryValue.companyTitle.string;
+    const isPopulated = (nameFull && email && companyTitle);
     return (
       <div className="statistic statistic--contact">
-        <span className="contact-name">Name</span><br/>
-        <span className="contact-email">email@somewhere.com</span><br/>
-        <span className="contact-title">Company Title</span>
+        {showLabel && <h4>{label}</h4>}
+        {isPopulated && <ul>
+          <li className="contact-name">{nameFull}</li>
+          {email && <li className="contact-email">{email}</li>}
+          {companyTitle && <span className="contact-title">{companyTitle}</span>}
+        </ul>}
       </div>
     );
   }
