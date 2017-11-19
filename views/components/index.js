b95ec119e14915aac8f8a511bcd277f87c4229fd
Darren Anderson <dranders@colorado.edu>
2017-11-19 11:50:14 -0800

    Remove reference to deleted component.

--- views/components/index.js
+++ views/components/index.js
@@ -4,7 +4,6 @@ const Divider = require('./divider.jsx');
 const EntryText = require('./entry-text.jsx');
 const NavMain = require('./nav-main.jsx');
 const StatisticContact = require('./statistic-contact.jsx');
-const StatisticNumberAndYear = require('./statistic-number_and_year.jsx');
 const StatisticNumber = require('./statistic-number.jsx');
 const StatisticString = require('./statistic-string.jsx');
 const StatisticSelect = require('./statistic-select.jsx');
@@ -14,7 +13,6 @@ module.exports = {
   EntryText,
   NavMain,
   StatisticContact,
-  StatisticNumberAndYear,
   StatisticNumber,
   StatisticString,
   StatisticSelect,
