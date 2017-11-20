72a4301d702e45e2c10470b87fa8164422d4d85b
Darren Anderson <dranders@colorado.edu>
2017-11-20 06:58:46 -0800

    Display table of partnerships.

--- views/partnerships.jsx
+++ views/partnerships.jsx
@@ -1,11 +1,38 @@
+require('dotenv').config();
+const debug = require('debug')('kdb:views:partnerships');
+
 const React = require('react');
 const JurisdictionLayout = require('./layouts/jurisdiction.jsx');
 
+const initiativeFields = {
+  name: { type: 'string', label: 'Name' },
+  description: { type: 'html', label: 'description' },
+  partners: { type: 'string', label: 'Partners' },
+  fundingSource: { type: 'string', label: 'Funding Source' },
+  fundingAmount: { type: 'string', label: 'Funding Amount' },
+  initiativeType: { type: 'string', label: 'Initiative Type' },
+  initiativeStatus: { type: 'string', label: 'Initiative Status' },
+};
+
+function Initiative(initiative) {
+  return Object.entries(initiative).map(([fieldId, fieldObj]) => {
+    if (fieldId === 'name') return <tr><td colspan="2">{fieldObj.string}</td></tr>;
+    if (!initiativeFields[fieldId]) return '';
+    if ((initiativeFields[fieldId].type === 'html') && fieldObj.html) return <tr><td>{initiativeFields[fieldId].label}</td><td dangerouslySetInnerHTML={{ __html: fieldObj.html }}></td></tr>;
+    if ((initiativeFields[fieldId].type === 'string') && fieldObj.string) return <tr><td>{initiativeFields[fieldId].label}</td><td>{fieldObj.string}</td></tr>;
+    return '';
+  });
+}
+
 class Page extends React.Component {
   render() {
+    const entry = this.props.data.jurisdiction.fields.initiatives || {};
+    const initiatives = entry.values || [];
     return (
       <JurisdictionLayout {...this.props}>
-        <div>Hello {this.props.name}</div>
+        <table><tbody>
+          {initiatives.map(initiative => <Initiative {...initiative}/>)}
+        </tbody></table>
       </JurisdictionLayout>
     );
   }
